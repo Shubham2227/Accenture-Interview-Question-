4f7bb5d8d2826c739ae587fa0321eb82e771bb5e
Shubham Kumar Maurya <33783005+Shubham2227@users.noreply.github.com>
2024-09-23 16:55:26 +0530

    Create Bitwise Operation.py

---
+++ All Coding Problem Solution/Bitwise Operation.py
@@ -0,0 +1,14 @@
+You are given a two integer A and B your task is to find and return an integer representing the value of thier bitwise OR operation 
+
+a= 5
+b=9
+output = 13
+
+def add_using_or(a, b):
+    return a | b
+
+# Test the function
+a = 15  # Binary: 1111
+b = 27  # Binary: 11011
+output = add_using_or(a, b)
+print(output)  # Output: 31 (Binary: 11111)
